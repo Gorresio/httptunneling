858f9ae1fd226a180dd5ede167058132d0f11acd
Stefano Gorresio <30286957+Gorresio@users.noreply.github.com>
2024-01-02 20:33:39 +0100

    Add files via upload

--- examples/random_stream_server.py
+++ examples/random_stream_server.py
@@ -6,26 +6,26 @@ import random
 import threading
 import httptunneling
 
-# initialize client
-client = httptunneling.HttpTunnelingSocketServer(8080)
+# initialize server
+server = httptunneling.HttpTunnelingSocketServer(8080)
 
 
-def send_task(client):
+def send_task(server):
     # send 8 random alphanumeric characters every second
     while True:
         data = ''.join([random.choice('QWERTYUIOPASDFGHJKLZXCVBNM0123456789') for _ in range(8)])
-        client.send(data)
+        server.send(data)
         print('sent: ' + data)
         time.sleep(1)
 
 
-def recv_task(client):
+def recv_task(server):
     # receive 8 random alphanumeric and print them (8 for consistency with send_task, but is not necessary)
     while True:
-        data = client.recv(8)
+        data = server.recv(8)
         print('recv: ' + str(data))
 
 
 # start threads
-threading.Thread(target=send_task, args=(client,)).start()
-threading.Thread(target=recv_task, args=(client,)).start()
+threading.Thread(target=send_task, args=(server,)).start()
+threading.Thread(target=recv_task, args=(server,)).start()
